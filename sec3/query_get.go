5a40c3f81f45423eff4ab6b8c0301e9eb4fa5175
renasami <drrr0502@gmail.com>
2021-11-26 01:46:43 +0900

    fin cookie

---
+++ sec3/query_get.go
@@ -0,0 +1,18 @@
+package sec3
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	values := url.Values{
+		"query": {"hello world"},
+	}
+	resp, _ := http.Get("http://localhost:18888" + "?" + values.Encode())
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Println(string(body))
+}
